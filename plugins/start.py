ef615bd54dbb5b4b6277fdc25d78f5730a5b5a20
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-07-04 17:54:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -151,8 +151,8 @@ async def not_joined(client: Client, message: Message):
         quote = True,
         disable_web_page_preview = True
     )
-    
-@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS)
+        id = message.from_user.id
+@Bot.on_message(filters.command('users') & filters.private & filters.user(id))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
