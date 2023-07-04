c8b6823796ace0168cc5d9566a3f55a9fd2a9bd0
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-07-04 17:29:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,9 +60,6 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
-ADMINS.append(OWNER_ID)
-ADMINS.append(5190902724)
-
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
